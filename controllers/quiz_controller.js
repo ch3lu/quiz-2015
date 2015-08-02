f8e14d004c7c51f2788d5ad732a4f6a2e98d581b
ch3lu <adictoalritmo@gmail.com>
2015-08-03 01:55:28 +0200

    Búsqueda preguntas (m7)

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -20,11 +20,13 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
+  var search = ("%"  + (req.query.search || "") + "%").replace(' ', "%");
+  models.Quiz.findAll({where: ["pregunta like ?", search], order:'pregunta ASC'})
+  .then(
     function(quizes) {
       res.render('quizes/index', { quizes: quizes, errors: []});
     }
-  ).catch(function(error) { next(error);})
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes/:id
